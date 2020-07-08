cc3e985206f75819d5d3988cba188092a2cadd3f
Martin Angers <martin.n.angers@gmail.com>
2020-07-07 22:32:05 -0400

    work on exec

--- shell.lua
+++ shell.lua
@@ -8,8 +8,8 @@
 -- Also, this can compose well:
 --   sh.v{'name', sh.v{'other', 'value'}}
 --
--- Execute a command, returns true on success, false (nil) and
--- an error message on failure:
+-- Execute a command, returns true on success, false otherwise and
+-- the command's status and exit code as 2nd and 3rd values:
 --   sh('cmd') or sh.exec('cmd') or sh'cmd'
 --
 -- This is just a wrapper over creating a command and exec'ing it:
@@ -31,8 +31,11 @@
 --   sh.test[[-f /path/to/file]]
 --]]
 
+local posix = require 'posix'
+
 local Shell, Cmd, Pipe = {}, {_name = 'Cmd'}, {_name = 'Pipe'}
 
+-- Constructor function for new Cmd or Pipe instances.
 local function newobj(class, o)
   o = o or {}
   class.__index = class
@@ -44,23 +47,29 @@ end
 -- results in a Pipe with those commands, flattened.
 local function __bor(l, r)
   local res
-  if type(l) == 'Pipe' then
+  local ltyp, rtyp = Shell.type(l), Shell.type(r)
+  if ltyp == 'Pipe' then
     res = l
-  else
+  elseif ltyp == 'Cmd' then
     res = newobj(Pipe)
     table.insert(res, l)
+  else
+    error('left operand must be a Pipe or a Cmd')
   end
 
-  local rtyp = type(r)
   if rtyp == 'Pipe' then
     for _, v in ipairs(r) do
       table.insert(res, v)
     end
-  else
+  elseif rtyp == 'Cmd' then
     table.insert(res, r)
+  else
+    error('right operand must be a Pipe or a Cmd')
   end
   return res
 end
+
+-- set the | operator metamethod on Cmd and Pipe metatables.
 Cmd.__bor, Pipe.__bor = __bor, __bor
 
 -- Calling a Shell object executes the value passed to it. If it is a string,
@@ -73,6 +82,9 @@ end
 
 setmetatable(Shell, {_name = 'Shell', __call = __call})
 
+-- Executes the provided command, which may be a string, a Cmd
+-- or a Pipe. Extra arguments are passed to the execution of
+-- the command (or pipe).
 function Shell.exec(...)
   -- TODO: is it better with select?
   local args = table.pack(...)
@@ -90,12 +102,17 @@ function Shell.exec(...)
   end
 end
 
+-- Returns the type of v, which expands the built-in type Lua
+-- function to include Shell, Cmd and Pipe instances.
 function Shell.type(v)
   local mt = getmetatable(v)
   if mt and mt._name then return mt._name end
   return type(v)
 end
 
+-- Returns the value of the environment variable. If t is a table
+-- instead of a string, then it returns the first non-empty string
+-- value if the initial environment variable name is not set.
 function Shell.var(t)
   if type(t) == 'string' then return os.getenv(t) end
   assert(type(t) == 'table', 'argument 1 must be a table or a string')
@@ -120,8 +137,18 @@ function Shell.test(cond)
   return Shell.exec('test ' .. cond)
 end
 
-function Cmd:exec(...)
+local function cmd_to_task(cmd, ...)
+  local task = {table.unpack(cmd, 1, cmd.n)}
+  local args = table.pack(...)
+  for i = 1, args.n do
+    table.insert(task, args[i])
+  end
+  return task
+end
 
+function Cmd:exec(...)
+  local _, status, code = assert(posix.spawn(cmd_to_task(self, ...)))
+  return (status == 'exited' and code == 0), status, code
 end
 
 function Cmd:output(...)
@@ -129,7 +156,22 @@ function Cmd:output(...)
 end
 
 function Pipe:exec(...)
+  assert(#self > 0, 'empty Pipe')
 
+  local tasks = {}
+  for i = 1, #self do
+    local t
+    if i == 1 then
+      t = cmd_to_task(self[i], ...)
+    else
+      t = cmd_to_task(self[i])
+    end
+    table.insert(tasks, t)
+  end
+
+  local pfd = posix.popen_pipeline(tasks, 'r')
+  local _, status, code = assert(posix.pclose(pfd))
+  return (status == 'exited' and code == 0), status, code
 end
 
 function Pipe:output(...)
